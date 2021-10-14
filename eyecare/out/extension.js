c4119240892923163d03fb19072a14898c48460b
Anup Kumar <anukumar@microsoft.com>
2021-10-14 18:02:08 +0530

    added timer page

--- eyecare/out/extension.js
+++ eyecare/out/extension.js
@@ -6,13 +6,22 @@ exports.deactivate = exports.activate = exports.showNotificationAlert = void 0;
 const vscode = require("vscode");
 let activeJob = null;
 function showNotificationAlert(message, showExcercisePage, keepItRunningAlways) {
-    vscode.window.showInformationMessage(message + ``, {
+    vscode.window
+        .showInformationMessage(message + ``, {
         modal: true,
+    }, ...["Ok"])
+        .then(selection => {
+        if (selection === "Ok") {
+            const panel = vscode.window.createWebviewPanel('eyecare', 'Blink your eyes', vscode.ViewColumn.One, { enableScripts: true });
+            panel.webview.html = getWebviewContent();
+            const timeout = setTimeout(() => panel.dispose(), 21000);
+            panel.onDidDispose(() => {
+                clearTimeout(timeout);
+            }, null);
+        }
     });
     if (showExcercisePage) {
-        const panel = vscode.window.createWebviewPanel('eyecare', 'Eye Excercise', vscode.ViewColumn.One, {});
-        // And set its HTML content
-        panel.webview.html = getWebviewContent();
+        //TODO
     }
     if (!keepItRunningAlways) {
         clearInterval(activeJob);
@@ -20,15 +29,206 @@ function showNotificationAlert(message, showExcercisePage, keepItRunningAlways) 
 }
 exports.showNotificationAlert = showNotificationAlert;
 function getWebviewContent() {
-    return `<!DOCTYPE html>
-<html lang="en">
+    return `
+
+<!DOCTYPE html>
+<html lang="en" >
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Excercise Page</title>
+  <meta charset="UTF-8">
+  <title>CodePen - JS Countdown Timer</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+  
+<style type="text/css">
+  
+      body {
+      font-family: sans-serif;
+      display: grid;
+      height: 100vh;
+      place-items: center;
+    }
+
+    .base-timer {
+      position: relative;
+      width: 300px;
+      height: 300px;
+    }
+
+    .base-timer__svg {
+      transform: scaleX(-1);
+    }
+
+    .base-timer__circle {
+      fill: none;
+      stroke: none;
+    }
+
+    .base-timer__path-elapsed {
+      stroke-width: 7px;
+      stroke: grey;
+    }
+
+    .base-timer__path-remaining {
+      stroke-width: 7px;
+      stroke-linecap: round;
+      transform: rotate(90deg);
+      transform-origin: center;
+      transition: 1s linear all;
+      fill-rule: nonzero;
+      stroke: currentColor;
+    }
+
+    .base-timer__path-remaining.green {
+      color: orange;
+    }
+
+    .base-timer__path-remaining.orange {
+      color: orange;
+    }
+
+    .base-timer__path-remaining.red {
+      color: rgb(65, 184, 131);
+    }
+
+    .base-timer__label {
+      position: absolute;
+      width: 300px;
+      height: 300px;
+      top: 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 48px;
+    }
+</style>
 </head>
 <body>
-    <img src="https://uploads-ssl.webflow.com/5ff8126e9f80b718dc63f907/60ed6b991608b628bd642cb5_How%20to%20Exercise%20Eyes.jpg" width="500" />
+<!-- partial:index.partial.html -->
+<div id="app"></div>
+<!-- partial -->
+  <script type="text/javascript">
+      // Credit: Mateusz Rybczonec
+
+      const FULL_DASH_ARRAY = 283;
+      const WARNING_THRESHOLD = 10;
+      const ALERT_THRESHOLD = 5;
+
+      const COLOR_CODES = {
+        info: {
+          color: "green"
+        },
+        warning: {
+          color: "orange",
+          threshold: WARNING_THRESHOLD
+        },
+        alert: {
+          color: "red",
+          threshold: ALERT_THRESHOLD
+        }
+      };
+
+      const TIME_LIMIT = 20;
+      let timePassed = 0;
+      let timeLeft = TIME_LIMIT;
+      let timerInterval = null;
+      let remainingPathColor = COLOR_CODES.info.color;
+
+      document.getElementById("app").innerHTML = \`
+      <div class="base-timer">
+        <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+          <g class="base-timer__circle">
+            <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+            <path
+              id="base-timer-path-remaining"
+              stroke-dasharray="283"
+              class="base-timer__path-remaining \${remainingPathColor}"
+              d="
+                M 50, 50
+                m -45, 0
+                a 45,45 0 1,0 90,0
+                a 45,45 0 1,0 -90,0
+              "
+            ></path>
+          </g>
+        </svg>
+        <span id="base-timer-label" class="base-timer__label">\${formatTime(
+          timeLeft
+        )}</span>
+      </div>
+      \`;
+
+      startTimer();
+
+      function onTimesUp() {
+        clearInterval(timerInterval);
+      }
+
+      function startTimer() {
+        timerInterval = setInterval(() => {
+          timePassed = timePassed += 1;
+          timeLeft = TIME_LIMIT - timePassed;
+          document.getElementById("base-timer-label").innerHTML = formatTime(
+            timeLeft
+          );
+          setCircleDasharray();
+          setRemainingPathColor(timeLeft);
+
+          if (timeLeft === 0) {
+            onTimesUp();
+          }
+        }, 1000);
+      }
+
+      function formatTime(time) {
+        const minutes = Math.floor(time / 60);
+        let seconds = time % 60;
+
+        if (seconds < 10) {
+          seconds = \`0\${seconds}\`;
+        }
+
+        return \`\${minutes}:\${seconds}\`;
+      }
+
+      function setRemainingPathColor(timeLeft) {
+        const { alert, warning, info } = COLOR_CODES;
+        if (timeLeft <= alert.threshold) {
+          document
+            .getElementById("base-timer-path-remaining")
+            .classList.remove(warning.color);
+          document
+            .getElementById("base-timer-path-remaining")
+            .classList.add(alert.color);
+        } else if (timeLeft <= warning.threshold) {
+          document
+            .getElementById("base-timer-path-remaining")
+            .classList.remove(info.color);
+          document
+            .getElementById("base-timer-path-remaining")
+            .classList.add(warning.color);
+        }
+      }
+
+      function calculateTimeFraction() {
+        const rawTimeFraction = timeLeft / TIME_LIMIT;
+        return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+      }
+
+      function setCircleDasharray() {
+        const circleDasharray = \`\${(
+          calculateTimeFraction() * FULL_DASH_ARRAY
+        ).toFixed(0)} 283\`;
+        document
+          .getElementById("base-timer-path-remaining")
+          .setAttribute("stroke-dasharray", circleDasharray);
+      }
+
+      //startTimeout();
+
+      function startTimeout(){
+         setTimeout("window.close();", 21000);
+      }
+
+  </script>
 </body>
 </html>`;
 }
